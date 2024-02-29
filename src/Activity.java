baab8a6778936136925c7b312ff3153012b8b529
Shyambatchu <107335320+Shyambatchu@users.noreply.github.com>
2024-02-29 12:13:31 +0530

    Create Activity.java

---
+++ src/Activity.java
@@ -0,0 +1,35 @@
+class Activity {
+    private String name;
+    private String description;
+    private double cost;
+    private int capacity;
+    private Destination destination;
+
+    public Activity(String name, String description, double cost, int capacity, Destination destination) {
+        this.name = name;
+        this.description = description;
+        this.cost = cost;
+        this.capacity = capacity;
+        this.destination = destination;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+}
